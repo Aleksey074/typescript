a6af34221cf49eedf7cb94a476dd2ca1f4b9e9a7
Aleksey074 <94966508+Aleksey074@users.noreply.github.com>
2022-07-16 20:35:30 +0500

    Delete dz-3 directory

--- dz-3/src/helpers/add-days.ts
+++
@@ -1,6 +0,0 @@
-export function addDays(date: Date, countDays: number): Date {
-    const copyDate = new Date(date);
-    copyDate.setDate(copyDate.getDate() + countDays);
-  
-    return copyDate;
-  }
--- dz-3/src/types/types.ts
+++
@@ -1,5 +0,0 @@
-export type UserKey = 'user';
-export type FavoritesAmountKey = 'favoritesAmount';
-export type FavoritesItemsKey = 'favoriteItems';
-export type Active = 'active';
-export type SearchFormId = 'search-form';
